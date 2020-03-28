1f5718dc1b19b4c6b5f1d18076baccccd3f8ade8
RemonShehata <RemonGerges94@gmail.com>
2020-03-28 18:34:32 +0200

    solved task2

--- LabOne.kt
+++ LabOne.kt
@@ -1,11 +1,24 @@
 package com.iti.intake40.kotlindemo
 
 fun main() {
-    // region  task1
+    doTaskTwo()
+}
+
+fun doTaskOne() {
     println("Enter your name: ")
     val name = readLine()
     val userName = if (name.isNullOrBlank()) "anonymous" else name
     print("hello, $userName")
+}
 
-    //endregion
+fun doTaskTwo() {
+    val numbers = mutableListOf<Int>()
+    for (i in 1..100) {
+        val random = (0..100).random()
+        numbers.add(random)
+    }
+
+    for (number in numbers.filter { it <= 10 }) {
+        print("$number  ")
+    }
 }
